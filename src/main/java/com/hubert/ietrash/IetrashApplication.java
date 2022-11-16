3af263749f033164309e75f6ae23dad7c6ad0348
HubertKuch <kuchhubert@gmail.com>
2022-11-16 13:07:38 +0100

    Initialize

---
+++ src/main/java/com/hubert/ietrash/IetrashApplication.java
@@ -0,0 +1,13 @@
+package com.hubert.ietrash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IetrashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IetrashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hubert/ietrash/IetrashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hubert.ietrash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IetrashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
